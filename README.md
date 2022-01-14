1708cd68b9c6230ee05f7cc1d6b12074ac2cf41e
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2022-01-14 20:35:08 +0100

    Added analytics plot script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ebe-tools
-Tools for working with Ebe compiler.
+# Ebe tools
+
+This repository contains developer and advanced user tools for working with [Ebe](https://github.com/mark-sed/ebe).
+
+## Analytics
+
+Contains scripts for working with analytics (`-a`) output from Ebe.
+
+### plot.py
+
+This script can be used to plot (to display and to a file) `-a` output. To see more info on its usage run
+it with `-h` argument.
---
+++ analytics/plot.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Script for plotting Ebe's analytics (-a) output.
+
+"""
+__author__ = "Marek Sedlacek"
+__date__ = "September 2021"
+
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+def print_help():
+    """
+    Prints script usage info and exits with success
+    """
+    print("Use: {} data.csv <output_image> <xlabel> <ylabel> <title>".format(sys.argv[0]))
+    exit(0)
+
+
+if __name__ == "__main__":
+    # Argument handeling
+    if len(sys.argv) <= 1 or (len(sys.argv) == 2 and sys.argv[1] == "-h"):
+        print_help()
+
+    data = []
+    x_data = []
+    y_data = []
+    with open(sys.argv[1]) as csvf:
+        reader = csv.reader(csvf, delimiter=',')
+        for row in reader:
+            if int(row[0]) in x_data:
+                data.append((x_data, y_data))
+                x_data = []
+                y_data = []
+            x_data.append(int(row[0]))
+            y_data.append(float(row[1]))
+        data.append((x_data, y_data))
+
+    e_num = 1
+    for x, y in data:
+        plt.plot(x, y, label='Evolution {}'.format(e_num))
+        e_num += 1
+
+    if len(sys.argv) >= 4:
+        plt.xlabel(sys.argv[3])
+    if len(sys.argv) >= 5:
+        plt.ylabel(sys.argv[4])
+    if len(sys.argv) >= 6:
+        plt.title(sys.argv[5])
+    plt.ylim([0.0, 1.05])
+    plt.legend()
+    # Show graph and save it to a pdf
+    if len(sys.argv) >= 3:
+        plt.savefig(sys.argv[2])
+    plt.show()
